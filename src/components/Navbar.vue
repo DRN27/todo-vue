33dafc1d0ee8c6a9dba7cab5fad53d1f9026236b
Maxim Padolski <47753504+DRN27@users.noreply.github.com>
2020-06-05 08:50:09 +0300

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="blue darken-1">
+        <div class="nav-wrapper">
+            <router-link to="/" class="brand-logo left">Tasks</router-link>
+            <ul class="right hide-on-med-and-down">
+                <router-link
+                    tag="li"
+                    exact
+                    active-class="active"
+                    to="/"
+                >
+                    <a href="#">Create</a>
+                </router-link>
+
+                <router-link
+                    tag="li"
+                    exact
+                    active-class="active"
+                    to="/list"
+                >
+                    <a href="#">List</a>
+                </router-link>
+
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default  {}
+</script>
+
+<style lang="scss" scoped>
+    nav {
+        padding: 0 25px;
+    }
+</style>
